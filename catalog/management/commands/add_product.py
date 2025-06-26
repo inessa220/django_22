debf1d2189eb62fa1d9d527f1140b4502b89ac5e
inessa <inessa220@mail.ru>
2025-06-26 09:38:55 +0600

    24

--- catalog/management/commands/add_product.py
+++ catalog/management/commands/add_product.py
@@ -1,20 +1,25 @@
 from django.core.management.base import BaseCommand
 from catalog.models import Category, Product
 
+
 class Command(BaseCommand):
-    help = 'Add test product to the database'
+    help = "Add test product to the database"
 
     def handle(self, *args, **kwargs):
         # Удаление существующих данных
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        #Создание категорий
-        one = Category.objects.create(name='1', description='11')
-        two = Category.objects.create(name='Напитки')
+        # Создание категорий
+        one = Category.objects.create(name="1", description="11")
+        two = Category.objects.create(name="Напитки")
 
-        #Создание продуктов
-        product1 = Product.objects.create(name='Тест1', description='Описание1', category=one, price=10)
-        product2 = Product.objects.create(name='Тест2', description='Описание2', category=two, price=100)
+        # Создание продуктов
+        product1 = Product.objects.create(
+            name="Тест1", description="Описание1", category=one, price=10
+        )
+        product2 = Product.objects.create(
+            name="Тест2", description="Описание2", category=two, price=100
+        )
 
-        self.stdout.write(self.style.SUCCESS('Successfully added test products'))
+        self.stdout.write(self.style.SUCCESS("Successfully added test products"))
--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pathlib import Path
+from django.conf.global_settings import STATICFILES_DIRS
 
 
 load_dotenv(override=True)
